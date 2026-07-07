95efcdddb6647ed1c3843944de918b63a7f4ef50
troy323 <125133832+troy323@users.noreply.github.com>
2026-07-07 15:41:43 +0530

    Refactor output_monitor class structure
    Refactor output_monitor class for improved readability and efficiency.

--- src/test_bench/output_monitor.sv
+++ src/test_bench/output_monitor.sv
@@ -1,26 +1,23 @@
+
 class output_monitor;
 
   virtual ram_if.mo vif;
-  mailbox #(transaction) mbx_ms;
+  mailbox #(transaction) mbx_ms;                   
   transaction tr;
 
   function new(mailbox #(transaction) mbx_ms, virtual ram_if.mo vif);
-    this.mbx_ms = mbx_ms;
-    this.vif    = vif;
+    this.mbx_ms=mbx_ms;
+    this.vif=vif;
   endfunction
 
   task start();
-    $display("[OMON] Output monitor started at time %0t", $time);
+    repeat(3)@(vif.cb_om); 
     forever begin
+      tr=new;
+      tr.data_out=vif.cb_om.data_out;
+      $display("[OMON] data_out:%0d at time:%0t",tr.data_out,$time);
+      mbx_ms.put(tr);
       @(vif.cb_om);
-      if (vif.cb_om.read_enb) begin
-        @(vif.cb_om);                              // wait 1 cycle for registered output
-        tr = new();
-        tr.data_out = vif.cb_om.data_out;
-        mbx_ms.put(tr);
-        $display("[OMON] data_out:%0h at time %0t", vif.cb_om.data_out, $time);
-      end
     end
-  endtask
-
+  endtask 
 endclass
