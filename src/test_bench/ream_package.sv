36f8b5d05b455b6df86f564c878c8a4efd012c98
troy323 <125133832+troy323@users.noreply.github.com>
2026-07-07 15:44:44 +0530

    Create ream_package.sv

---
+++ src/test_bench/ream_package.sv
@@ -0,0 +1,13 @@
+package ram_package;
+ 
+ 
+  `include "transaction.sv"
+  `include "driver.sv"
+  `include "input_monitor.sv"
+  `include "output_monitor.sv"
+  `include "reference.sv"
+  `include "scoreboard.sv"
+  `include "generator.sv"
+  `include "environment.sv"
+  `include "test.sv"
+endpackage
