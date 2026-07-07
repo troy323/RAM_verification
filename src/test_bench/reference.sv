b7ab20bf53d84e13a481ff6da6eba81370091d0a
troy323 <125133832+troy323@users.noreply.github.com>
2026-07-07 15:38:06 +0530

    Update reference.sv

--- src/test_bench/reference.sv
+++ src/test_bench/reference.sv
@@ -1,3 +1,4 @@
+
 class reference;
 
   transaction tr;
@@ -13,24 +14,32 @@ class reference;
   endfunction
 
   task start();
-    $display("[REF] Reference model started at time %0t", $time);
     forever begin
       mbx_mr.get(tr);
-      if (tr.write_enb && !tr.read_enb) begin
+      exp = new();
+      if(!tr.reset) begin 
+        mem[tr.address]=0;
+        data_out<=8'bz;
+      end
+      else if (tr.write_enb && !tr.read_enb) begin
         mem[tr.address] = tr.data_in;
-        $display("[REF] Write data:%0h to address:%0d", tr.data_in, tr.address);
+        exp.data_out=8'h0;
+        $display("[REF] Write data:%0h to address:%0h at %0t", tr.data_in, tr.address,$time);
       end
       else if (tr.read_enb && !tr.write_enb) begin
-        exp = new();
         exp.address = tr.address;
         if (mem.exists(tr.address))
           exp.data_out = mem[tr.address];
-        else
-          exp.data_out = 8'bz;                     // FIX: DUT outputs z for unwritten addresses
-        mbx_rs.put(exp);
-        $display("[REF] Predicted read data:%0h from address:%0d", exp.data_out, exp.address);
+        else exp.data_out = 8'h0;
+        $display("[REF] Predicted read data:%0d from address:%0d at %0t", exp.data_out, exp.address,$time);
       end
-      // else: both 0 → IDLE, skip
+      else begin 
+        exp.data_out=0;
+        $display("No operation at time:%0t",$time);
+      end
+      mbx_rs.put(exp);
+      
+      
     end
   endtask
 
